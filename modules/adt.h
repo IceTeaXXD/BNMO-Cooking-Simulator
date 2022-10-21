043d8640318047e0e0071e0c0ec1149786df03e7
IceTeaXXD <16521516@mahasiswa.itb.ac.id>
2022-10-21 20:54:24 +0700

    added adt include all

---
+++ modules/adt.h
@@ -0,0 +1,13 @@
+#ifndef ADT_H
+#define ADT_H
+
+#include "./food/food.h"
+#include "./liststatik/liststatik.h"
+#include "./matrix/matrix.h"
+#include "./point/point.h"
+#include "./prioqueue/prioqueuetime.h"
+#include "./stack/stack.h"
+#include "./time/time.h"
+#include "./wordmachine/wordmachine.h"
+
+#endif
