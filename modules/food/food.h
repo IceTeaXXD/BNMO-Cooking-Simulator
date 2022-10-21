4d4e31f18372f44d67ea36a7c07cc91b3ae09ef4
IceTeaXXD <16521516@mahasiswa.itb.ac.id>
2022-10-21 20:06:49 +0700

    Added ADT food

---
+++ modules/food/food.h
@@ -0,0 +1,25 @@
+/* ADT Food */
+/*Memiliki data ID makanan, Nama makanan, Waktu kedaluwarsa, Lokasi aksi, dan Lama pengiriman.*/
+
+#ifndef FOOD_H
+#define FOOD_H
+
+#include "../utility/boolean.h"
+
+typedef struct
+{
+    int id;
+    char food_name;
+    int expiry_time;
+    char location;
+    int delivery_time;
+
+}food;
+
+#define ID(F) (F).id
+#define FOOD_NAME(F) (F).food_name
+#define EXPIRY_TIME(F) (F).expiry_time
+#define LOCATION(F) (F).location
+#define DELIVERY_TIME(F) (F).delivery_time
+
+#endif
