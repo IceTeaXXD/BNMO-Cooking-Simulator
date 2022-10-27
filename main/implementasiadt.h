2e563974c5859b41408ec8b00a623838610da978
AlphaThrone <haikalardzi@gmail.com>
2022-10-27 20:50:39 +0700

    main

---
+++ main/implementasiadt.h
@@ -0,0 +1,15 @@
+#ifndef IMPLEMENTASIADT_H
+#define IMPLEMENTASIADT_H
+
+#include "../modules/adt.h"
+#include "../modules/listdin/listdin.c"
+#include "../modules/liststatik/liststatik.c"
+#include "../modules/matrix/matrix.c"
+#include "../modules/point/point.c"
+#include "../modules/prioqueue/prioqueuetime.c"
+#include "../modules/stack/stack.c"
+#include "../modules/time/time.c"
+#include "../modules/tree/tree.c"
+
+
+#endif
