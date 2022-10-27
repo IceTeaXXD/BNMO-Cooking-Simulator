0acda13e6d37372bb3f218fff2035f93a5ef8d49
IceTeaXXD <16521516@mahasiswa.itb.ac.id>
2022-10-27 23:16:17 +0700

    resolved compiler error

--- modules/wordmachine/wordmachine.c
+++ modules/wordmachine/wordmachine.c
@@ -85,7 +85,8 @@ void CopyWord()
 
 int WordToInt(Word string){
    int result = 0;
-   for (int i=0; i<string.Length; i++){
+   int i;
+   for (i=0; i<string.Length; i++){
       result = result * 10 + (int) string.TabWord[i] - 48;
    }
    return result;
