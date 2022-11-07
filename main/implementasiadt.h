c9aaa9334207a57b8776fc1fdffcc1374a760557
IceTeaXXD <13521024@std.stei.itb.ac.id>
2022-11-07 16:12:29 +0700

    updated include

--- main/implementasiadt.h
+++ main/implementasiadt.h
@@ -5,12 +5,13 @@
 #include "../modules/listdin/listdin.c"
 #include "../modules/liststatik/liststatik.c"
 #include "../modules/point/point.c"
-//#include "../modules/prioqueue/prioqueueinv.c"
+#include "../modules/prioqueue/prioqueueinv.c"
 #include "../modules/stack/stack.c"
 #include "../modules/time/time.c"
 #include "../modules/tree/tree.c"
 #include "../modules/map/map.c"
 #include "../modules/wordmachine/wordmachine.c"
 #include "../modules/matrix/matrix.c"
+#include "../modules/food/food.c"
 
 #endif
